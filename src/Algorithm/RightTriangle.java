28899e7d1bb7e3cd96bae8290e6112c6143b0593
kahyun Lee <gmeoqq@gmail.com>
2022-10-17 09:11:21 +0900

    별찍기         *         **         ***         ****

---
+++ src/Algorithm/RightTriangle.java
@@ -0,0 +1,19 @@
+package Algorithm;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        for (int i = 0; i <5; i++ ) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+출력물
+        *
+        **
+        ***
+        ****
+*/
