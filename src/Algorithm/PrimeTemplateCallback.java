7eeac9941403ec25a53a37b91c11f93f259b93df
kahyun Lee <gmeoqq@gmail.com>
2022-11-01 10:39:31 +0900

    소수 templateCallback 적용 2단계(인터페이스, 구현클래스)

--- src/Algorithm/PrimeTemplateCallback.java
+++ src/Algorithm/PrimeTemplateCallback.java
@@ -4,12 +4,21 @@ package Algorithm;
 //방법 1. n의 수를 2~n-1수로 나눠서 remainder가 0이 아니면 소수
 //방법 2. n의 수를 n/2미만의 수로 나누어 보는 방법
 //방법 3. n의 수를 루트 n까지로 나누어 보는 방법
-public class PrimeTemplateCallback { //templateCallback 적용 1단계
-    boolean someOperation(int a,int b){
+
+
+interface StatementStrategy {
+    boolean compare(int a, int b);
+}
+class primeStrategy implements StatementStrategy {
+
+    @Override
+    public boolean compare(int a, int b) {
         return a<b;
     }
-    public boolean solution(int n) { //n사이의 있는 소수 출력
-        for (int i = 2; someOperation(i,n); i++) {
+}
+public class PrimeTemplateCallback { //templateCallback 적용 1단계
+    public boolean solution(int n,StatementStrategy st) {
+        for (int i = 2; st.compare(i,n); i++) {
             if (n%i!=0) return false;
         }
         return true;
@@ -17,7 +26,7 @@ public class PrimeTemplateCallback { //templateCallback 적용 1단계
 
     public static void main(String[] args) {
         PrimeTemplateCallback prime = new PrimeTemplateCallback();
-        System.out.println(prime.solution(13));
+        System.out.println(prime.solution(13,));
     }
 
 }
