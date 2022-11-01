d16d51140c3f165e329b12ed6d5cf66c14139fb5
kahyun Lee <gmeoqq@gmail.com>
2022-11-01 09:32:20 +0900

    소수 1번 방식 (수정)

--- src/Algorithm/Prime.java
+++ src/Algorithm/Prime.java
@@ -2,12 +2,16 @@ package Algorithm;
 
 import java.util.Arrays;
 
-public class Prime {
-    public int[] solution(int n) {
-        int[] answer = {};
+//소수 : 1, 자기 자신으로만 나눠지는 수
+//방법 1. n의 수를 2~n-1수로 나눠서 remainder가 0이 아니면 소수
+//방법 2. n의 수를 n/2미만의 수로 나누어 보는 방법
+//방법 3. n의 수를 루트 n까지로 나누어 보는 방법
+public class Prime { //문제에서 int로 받으라고해서 수정
+    public int solution(int n) {
+        int answer = 0;
         for (int i = 2; i < n-1; i++) {
             if (n%i!=0) {
-                answer= new int[]{n};
+                answer= n;
             }
         }
         return answer;
@@ -15,6 +19,6 @@ public class Prime {
 
     public static void main(String[] args) {
         Prime prime = new Prime();
-        System.out.println(Arrays.toString(prime.solution(13)));
+        System.out.println(prime.solution(13));
     }
 }
