cda33d6eee18de26da1c944f8997dcd8073d8522
kahyun Lee <gmeoqq@gmail.com>
2022-11-02 10:16:53 +0900

    에라토스테네스 list(속도문제)

--- src/Algorithm/Eratosthenes2.java
+++ src/Algorithm/Eratosthenes2.java
@@ -4,25 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Eratosthenes2 {
-
-    public static void main(String[] args) {
-
+    public int solution(int n){
         //2부터 50까지 숫자가 들어있는 49칸짜리 배열(리스트) 만들기
-        int n = 50;
         List<Integer> nums = new ArrayList<Integer>();
         for (int i = 2; i <= n; i++) {
             nums.add(i);
         }
-        //만든 배열에서 2를 제외한 모든 2의 배수 지우기
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums.get(i) % 2 == 0 && nums.get(i) != 2) {
-                nums.remove(i);
+        //만든 배열에서 모든 배수 지우기
+        for (int j = 2; j * j <= n; j++ ) {
+            for (int i = 0; i < nums.size(); i++) {
+                if (nums.get(i) % j == 0 && nums.get(i) > j) {
+                    nums.remove(i);
+                }
             }
         }
-
-//        nums.removeIf(nums -> nums % 2 == 0 && nums != 2); 써보고싶다!!!
-        //배열에 있는 원소의 개수, 원소 출력해보기
-        System.out.println(nums);
-        System.out.println(nums.size());
+        return nums.size();
     }
+//        nums.removeIf(nums -> nums % 2 == 0 && nums != 2); 써보고싶다!!!
 }
