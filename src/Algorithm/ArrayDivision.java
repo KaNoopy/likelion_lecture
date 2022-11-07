38c02ed076c1133ea9c08d0255f8404cfca9e392
kahyun Lee <gmeoqq@gmail.com>
2022-11-07 10:23:22 +0900

    나누어 떨어지는 숫자배열 완성

--- src/Algorithm/ArrayDivision.java
+++ src/Algorithm/ArrayDivision.java
@@ -1,11 +1,13 @@
 package Algorithm;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ArrayDivision {
-    public ArrayList<Integer> solution(int[] arr, int divisor) {
-
-        ArrayList<Integer> list = new ArrayList<Integer>();
+    public int[] solution(int[] arr, int divisor) {
+        // list조건에 맞게 넣기
+        List<Integer> list = new ArrayList<Integer>();
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] % divisor == 0) {
@@ -14,13 +16,21 @@ public class ArrayDivision {
                 list.add(-1);
             }
         }
-        return list;
+        //list array로 바꾸기
+        int[] answer = new int[list.size()];
+        int size =0;
+        for (Integer nums : list) {
+            answer[size++] = nums;
+        }
+        //오름차순 정렬
+        Arrays.sort(answer);
+        return answer;
     }
 
     public static void main(String[] args) {
         int[] arr = {5, 9, 7, 10};
         ArrayDivision a = new ArrayDivision();
-        System.out.println(a.solution(arr, 5));
+        System.out.println(Arrays.toString(a.solution(arr, 5)));
     }
 
 }
