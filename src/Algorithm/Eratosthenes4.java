44a3bcc3b0f14f00f3954abd226337d55e305569
kahyun Lee <gmeoqq@gmail.com>
2022-11-14 10:27:42 +0900

    에라토스테네스 채 (2를 제외한 2의 배수 0으로 바꾸기 미완성)

--- src/Algorithm/Eratosthenes4.java
+++ src/Algorithm/Eratosthenes4.java
@@ -1,28 +1,23 @@
 package Algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Eratosthenes4 {
-    public boolean solution(int n) {
-        //2부터 50까지 숫자가 들어있는 49칸짜리 배열(리스트) 만들기
-        int[] nums = new int[0];
-        for (int i = 2; i <= n; i++) {
-            nums = new int[i];
-        }
-
-        //만든 Array크기의 checks배열을 만듭니다.
-        int[] checks = new int[nums.length];
-
-        //만든 Array대신 checks배열에 2를 제외한 모든 2의 배수에 false로 바꾸는 로직
-        for (int i = 2; i < checks.length; i++) {
-            if (i % 2 == 0) {
-                return checks[i] = false;
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
-        //소수만 출력 해보기: checks에 true인 nums[i]만 출력
-        Eratosthenes4 eratosthenes3 = new Eratosthenes4();
-        eratosthenes3.solution(100);
+        //1. 2~50이 들어있는 배열 생성
+        int N = 50;
+        int[] nums = new int[N - 1];
+        for (int i = 2; i <= N; i++) {
+            nums[i] = i;
+        }
+
+        //2. 2를 제외한 2의  배수 -> 0으로 바꾸기
+        for (int i = 0; i <= i+2; i++) {
+                nums[i] = 0;
+        }
+
+        System.out.println(Arrays.toString(nums));
     }
 }
